da4e1033e4b27952c421a578c5625e627f303a51
LogicalShark <marcusa3.14@gmail.com>
2016-05-03 15:58:36 -0400

    removed require

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function setPixel(imageData, x, y, h, a) {
 }
 function combine()
 {
-	var Jimp = require("jimp");
 	var weights = [];
 	weights[0] = parseInt(document.getElementById("f1").value, 10);
 	weights[1] = parseInt(document.getElementById("f2").value, 10);
@@ -16,6 +15,7 @@ function combine()
 	weights[4] = parseInt(document.getElementById("f5").value, 10);
 	weights[5] = parseInt(document.getElementById("f6").value, 10);
 	var vout = 0;
+	console.log("asdf");
 	var hues = [0][0];
 	// https://github.com/oliver-moran/jimp
 	for(var n = 0; n<6; n++)
