c27092ddafbffde2ed527a2d08d0b32e4a0e08c9
LogicalShark <marcusa3.14@gmail.com>
2016-05-06 09:32:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -25,30 +25,6 @@ function combine()
 			hues[x][y] = 0;
 		}
 	}
-	// https://github.com/oliver-moran/jimp
-	// for(var n = 0; n<6; n++)
-	// {
-	// 	var m = n+1;
-	// 	//Resize and quality all
-	// 	Jimp.read("images/Layer "+m+".png").then(function (face) {
-	// 		face.resize(256, 256)            // resize
-	// 		.quality(60)                 // set JPEG quality
-	// 		.write("images/Layer "+m+".png"); // save
-	// 		}).catch(function (err) {
-	// 	console.error(err);
-	// 	});
-	// 	//Scan and record hue values
-	// 	Jimp.read("images/Layer "+m+".png").then(function (face) {
-	// 		face.scan(0, 0, face.bitmap.width, face.bitmap.height, function (x, y, idx) {
-	// 			// x, y is the position of this pixel on the image
-	// 			// idx is the position start position of this rgba tuple in the bitmap Buffer
-	// 			// this is the image
-	// 			hues[x][y] += this.bitmap.data[idx]*weights[n];
-	// 		})
-	// 		}).catch(function (err) {
-	// 	console.error(err);
-	// 	});
-	// }
 	
 	var c = document.createElement("canvas");
 	var cx = c.getContext("2d");
