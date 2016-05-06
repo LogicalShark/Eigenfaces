6c6389543796df6e1212611078b9228b77491e13
LogicalShark <marcusa3.14@gmail.com>
2016-05-06 11:29:16 -0400

    added random, 8 faces

--- main.js
+++ main.js
@@ -8,13 +8,35 @@ function setPixel(imageData, x, y, h, a) {
 function combine()
 {
 	var weights = [];
-	var size = 128;
 	weights[0] = parseInt(document.getElementById("f1").value, 10);
 	weights[1] = parseInt(document.getElementById("f2").value, 10);
 	weights[2] = parseInt(document.getElementById("f3").value, 10);
 	weights[3] = parseInt(document.getElementById("f4").value, 10);
 	weights[4] = parseInt(document.getElementById("f5").value, 10);
 	weights[5] = parseInt(document.getElementById("f6").value, 10);
+	weights[4] = parseInt(document.getElementById("f7").value, 10);
+	weights[5] = parseInt(document.getElementById("f8").value, 10);
+	makeFace(weights);
+}
+function random()
+{
+	var weights = [];
+	for(var x = 0; x<8; x++)
+	{
+		weights[x] = 2 * (random() - 0.5);
+	}
+	makeFace(weights);	
+}
+function makeFace(weights)
+{
+	// var weights = [];
+	var size = 128;
+	// weights[0] = parseInt(document.getElementById("f1").value, 10);
+	// weights[1] = parseInt(document.getElementById("f2").value, 10);
+	// weights[2] = parseInt(document.getElementById("f3").value, 10);
+	// weights[3] = parseInt(document.getElementById("f4").value, 10);
+	// weights[4] = parseInt(document.getElementById("f5").value, 10);
+	// weights[5] = parseInt(document.getElementById("f6").value, 10);
 	var vout = 0;
 	var hues = [];
 	for(x = 0; x<size; x++)
@@ -96,6 +118,32 @@ function combine()
 	cx.drawImage(document.getElementById("face6"), 0, 0, size, size);
 	var d = cx.getImageData(0,0,size,size);
 	var weight = document.getElementById("f6").value;
+	for (x = 0; x<size; x++) 
+	{
+		for(y = 0; y<size; y++)
+		{
+			index = ((x*(d.width*4)) + (y*4)) + 2;
+			hues[x][y]+=parseFloat(weight)*d.data[index];
+		}
+	}
+	var c = document.createElement("canvas");
+	var cx = c.getContext("2d");
+	cx.drawImage(document.getElementById("face7"), 0, 0, size, size);
+	var d = cx.getImageData(0,0,size,size);
+	var weight = document.getElementById("f7").value;
+	for (x = 0; x<size; x++) 
+	{
+		for(y = 0; y<size; y++)
+		{
+			index = ((x*(d.width*4)) + (y*4)) + 2;
+			hues[x][y]+=parseFloat(weight)*d.data[index];
+		}
+	}
+	var c = document.createElement("canvas");
+	var cx = c.getContext("2d");
+	cx.drawImage(document.getElementById("face8"), 0, 0, size, size);
+	var d = cx.getImageData(0,0,size,size);
+	var weight = document.getElementById("f8").value;
 	for (x = 0; x<size; x++) 
 	{
 		for(y = 0; y<size; y++)
