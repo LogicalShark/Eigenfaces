632eae94107d42f75ba7092614aa78fec0e19bb6
LogicalShark <marcusa3.14@gmail.com>
2016-05-06 11:40:06 -0400

    bigger vals

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function randomWeights()
 	var x;
 	for(x = 0; x<8; x++)
 	{
-		weights[x] = 2 * (Math.random() - 0.5);
+		weights[x] =  Math.sqrt(Math.sqrt(Math.random() - 0.5));
 	}
 	makeFace(weights);	
 }
